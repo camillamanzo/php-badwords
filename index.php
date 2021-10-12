3e59b1376653fbb079de04eecdeeed1eba802c50
camillamanzo <camillasouthafrica@gmail.com>
2021-10-12 16:02:43 +0200

    implemented str_replace

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php 
     $richardFeynman = "Richard Phillips Feynman (/ˈfaɪnmən/; May 11, 1918 – February 15, 1988) was an American theoretical physicist, known for his work in the path integral formulation of quantum mechanics, the theory of quantum electrodynamics, the physics of the superfluidity of supercooled liquid helium, as well as his work in particle physics for which he proposed the parton model. For contributions to the development of quantum electrodynamics, Feynman received the Nobel Prize in Physics in 1965 jointly with Julian Schwinger and Shin'ichirō Tomonaga.";
+    $richardFeynman2 = str_replace("physicist","***", $richardFeynman);
 ?>
 <html lang="en">
 <head>
@@ -13,5 +14,6 @@
     <h1>Richard Feynman</h1>
     <p><?php echo $richardFeynman ?></p>
     <p>Censured word: <?php echo $_GET["censured"] ?></p>
+    <p><?php echo $richardFeynman2 ?></p>
 </body>
 </html>
